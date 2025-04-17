6475ad8df7a211952f1ac6a70a956c3505fbf04c
VladiSH31 <vladislav.sheremet@ukr.net>
2025-04-17 13:06:08 +0300

    -creat part structure -made list and button section

--- main.js
+++ main.js
@@ -16,8 +16,32 @@ nameValueInput.setAttribute('type', 'text');
 nameValueInput.setAttribute('id', 'nameValueInput')
 nameValueInput.setAttribute('placeholder', 'Enter Name=Value pair');
 
-inputSection.append(nameValueInput)
-container.append(title, inputSection);
+let addButton = document.createElement('button');
+addButton.setAttribute('id', 'addButton');
+addButton.innerText = 'Add';
+
+let listSection = document.createElement('div');
+listSection.classList.add('list-section')
+
+let nameValueList = document.createElement('select');
+nameValueList.setAttribute('id', 'nameValueList');
+
+let buttonSection = document.createElement('div');
+buttonSection.classList.add('button-section');
+
+let sortByNameButton = document.createElement('button');
+sortByNameButton.setAttribute('id', 'sortByNameButton');
+
+let sortByValueButton = document.createElement('button');
+sortByValueButton.setAttribute('id', 'sortByValueButton');
+
+let deleteButton = document.createElement('button');
+deleteButton.setAttribute('id', 'deleteButton')
+
+buttonSection.append(sortByNameButton, sortByValueButton, deleteButton)
+listSection.append(nameValueList, buttonSection)
+inputSection.append(nameValueInput, addButton)
+container.append(title, inputSection, listSection);
 mainDiv.appendChild(container);
 
 // Функції
